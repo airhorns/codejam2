f1ef0de8bc7b38fcd079bc59a1cc49d6cf5fb02e
Ilia Denotkine <denotkine@gmail.com>
2011-11-20 18:30:38 -0500

    removed libevent

--- C_server/make.sh
+++ C_server/make.sh
@@ -1,4 +1,4 @@
 gcc -std=c99 -Wall -fPIC -g -ggdb -O2 -I../src/ -I/usr/include -I/usr/local/include -I./ -D_GNU_SOURCE -c -o server.o server.c  
-gcc -std=c99 -Wall -fPIC -g -ggdb -O2 -I../src/ -I/usr/include -I/usr/local/include -I./ -D_GNU_SOURCE -o index.cgi server.o libhiredis.a libqdecoder.a -levent -lfcgi 
+gcc -std=c99 -Wall -fPIC -g -ggdb -O2 -I../src/ -I/usr/include -I/usr/local/include -I./ -D_GNU_SOURCE -o index.cgi server.o libhiredis.a libqdecoder.a -lfcgi 
 sudo cp index.cgi /home/www/cgi/
 sudo chmod +x /home/www/cgi/ 
