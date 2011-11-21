5476953367e6ca410db871b3ad3374e747c6d031
Ilia Denotkine <denotkine@gmail.com>
2011-11-20 19:22:54 -0500

    start script

--- go_baby_go.sh
+++ go_baby_go.sh
@@ -1,10 +1,10 @@
-ruby exchange_server.rb --daemonize --port=9000 --environment=production
-pid=`cat goliath.pid`
-echo "Exchange PID: $pid"
+#ruby exchange_server.rb --daemonize --port=9000 --environment=production
+#pid=`cat goliath.pid`
+#echo "Exchange PID: $pid"
 ruby notification_daemon.rb start
 pid=`ruby notification_daemon.rb status | cut -d[ -f2 | cut -d" " -f 2 | cut -d] -f1`
 echo "Notification server PID: $pid"
 thin start --daemonize
 pid=`cat tmp/pids/thin.pid`
 echo "User server PID: $pid"
-
+sudo spawn-fcgi -a127.0.0.1 -p8500 -n /home/www/cgi/index.cgi -C 12 
